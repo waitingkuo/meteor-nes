9beecf1ed4cf1627bac5ad22a135dcca0942abd6
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-10-11 17:06:14 +0800

    add more roms

--- client/index.js
+++ client/index.js
@@ -2,10 +2,33 @@ if (Meteor.isClient) {
   Template.index.onRendered(function() {
     window.nes = new JSNES({
       'ui': $('#emulator').JSNESUI({
+        /*
         "Homebrew": [
           ['Concentration Room', '/packages/jsnes/jsnes/roms/croom/croom.nes'],
           ['LJ65', '/packages/jsnes/jsnes/roms/lj65/lj65.nes'],
         ],
+        */
+        "Roms": [
+                    ['Bubble Bobble', 'roms/Bubble Bobble (U).nes'],
+                    
+                    ['Contra', 'roms/Contra (U) [!].nes'],
+                    ['Donkey Kong', 'roms/Donkey Kong (JU).nes'],
+                    ['Dr. Mario', 'roms/Dr. Mario (JU).nes'],
+                    ['Golf', 'roms/Golf (JU).nes'],
+                    ['The Legend of Zelda', 'roms/Legend of Zelda, The (U) (PRG1).nes'],
+                    ['Lemmings', 'roms/Lemmings (U).nes'],
+                    ['Lifeforce', 'roms/Lifeforce (U).nes'],
+                    
+                    ['Mario Bros.', 'roms/Mario Bros. (JU) [!].nes'],
+                    ['Mega Man', 'roms/Mega Man (U).nes'],
+                    ['Pac-Man', 'roms/Pac-Man (U) [!].nes'],
+                    ['Super Mario Bros.', 'roms/Super Mario Bros. (JU) (PRG0) [!].nes'],
+                    ['Super Mario Bros. 3', 'roms/Super Mario Bros. 3 (U) (PRG1) [!].nes'],
+                    ['Tennis', 'roms/Tennis (JU) [!].nes'],
+                    ['Tetris', 'roms/Tetris (U) [!].nes'],
+                    ['Tetris 2', 'roms/Tetris 2 (U) [!].nes'],
+                    ['Zelda II - The Adventure of Link', 'roms/Zelda II - The Adventure of Link (U).nes']
+          ],
       }),
     })
     let did = false;
